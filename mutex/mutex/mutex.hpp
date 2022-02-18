773ba23346d56588d7c780b707938c550fec198a
Alexandr Tskhay <>
2022-02-18 17:51:30 +0300

    Easier way

--- mutex/mutex/mutex.hpp
+++ mutex/mutex/mutex.hpp
@@ -12,26 +12,14 @@ class Mutex {
   }
 
   void Lock() {
-    uint32_t need = 0u;
-    flag_.compare_exchange_strong(need, 1);
-    if (need != 0) {
-      do {
-        uint32_t expected = 1u;
-        flag_.compare_exchange_strong(expected, 2);
-        if (need == 2 || expected != 0) {
-          flag_.FutexWait(2);
-        }
-        need = 0;
-        flag_.compare_exchange_strong(need, 2);
-      } while (need != 0);
+    while (flag_.exchange(1) == 1) {
+      flag_.FutexWait(1);
     }
   }
 
   void Unlock() {
-    if (flag_.fetch_sub(1) != 1) {
-      flag_.store(0);
-      flag_.FutexWakeOne();
-    }
+    flag_.store(0);
+    flag_.FutexWakeOne();
   }
 
  private:
